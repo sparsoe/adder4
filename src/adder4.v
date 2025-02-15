952bfb1c0a492f13371d817348f626032380c1aa
sparsoe <sparsoe@users.noreply.github.com>
2025-02-15 16:30:42 +0100

    Update adder4.v
    dropped carry in and out

--- src/adder4.v
+++ src/adder4.v
@@ -1,7 +1,5 @@
 module fulladd (  input [3:0] a,
                   input [3:0] b,
-                  input c_in,
-                  output c_out,
                 output [3:0] sum);
 
    assign {c_out, sum} = a + b + c_in;
